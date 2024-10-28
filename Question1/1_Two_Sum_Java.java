63405b9b9b75b75fb21b3a4adfe511b7fa394937
alexlee5124 <59184872+alexlee5124@users.noreply.github.com>
2024-10-28 11:00:01 -0400

    Update 1_Two_Sum_Java.java

--- Question1/1_Two_Sum_Java.java
+++ Question1/1_Two_Sum_Java.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+        int complement;
+        for (int i = 0 ; i < n ; i++) {
+            complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            else {
+                numMap.put(nums[i], i);
+            }
+        }     
+        return new int[]{};
+    }
+}
