d9dc997a09da7e8d6be3d141e4f5a0666e372088
alexlee5124 <59184872+alexlee5124@users.noreply.github.com>
2024-10-29 05:58:52 -0400

    Update 9_Palindrome_Number_attempt1.py

--- 9/9_Palindrome_Number_attempt1.py
+++ 9/9_Palindrome_Number_attempt1.py
@@ -1,1 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        input = str(x)
+        for i in range(len(input)):
+            if input[i] != input[-(i+1)]:
+                return False
 
+        return True
